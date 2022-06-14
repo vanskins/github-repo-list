7e2a134af14a0c0809403790f4c6eefa1a470dd3
Van Alfred <vansabacajan@gmail.com>
2022-06-14 19:55:47 +0800

    update code fix

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ import Api from './api'
 
 function App() {
   const [state, setState] = useState({
-    username: '',
+    repo: '',
     repositories: []
   })
   const fetchRepositories = () => {
-    Api().get(`/users/${state.username}/repos`)
+    Api().get(`/search/repositories?q=${state.repo}`)
     .then(success => {
+      console.log(success)
       if (success && success.request && success.request.status === 200) {
-        setState({ ...state, repositories: success.data})
+        setState({ ...state, repositories: success.data.items})
       }
     })
     .catch(err => {
@@ -22,26 +23,30 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Github repository finder using username</h1>
+        <h1>Github repository finder</h1>
         <div className="input-container">
-        <input className="text-input" value={state.username} onChange={(e) => {
+        <input className="text-input" value={state.repo} onChange={(e) => {
           const { value } = e.target
-          setState({ ...state, username: value })
-        }} placeholder="username" />
-        <button className={state.username.length > 0 ? "button": "button-disabled"} disabled={!state.username.length > 0} onClick={() => fetchRepositories()}>Submit</button>
+          setState({ ...state, repo: value })
+        }} placeholder="repository name" />
+        <button className={state.repo.length > 0 ? "button": "button-disabled"} disabled={!state.repo.length > 0} onClick={() => fetchRepositories()}>Submit</button>
         </div>
-        {
+      </header>
+      <div className="repo-container">
+      {
           state.repositories.map((item, key) => {
             return (
-              <div className="repo-card" key={key}>
+            <div className="repo-card" key={key}>
               <p>Repository: <b>{item.full_name}</b></p>
+              <p>Forks ({item.forks})</p>
+              <p>Stars ({item.stargazers_count})</p>
               <p>Click to open repository </p>
               <a href={item.html_url} target="_blank" rel="noreferrer noopener">{item.html_url}</a>
             </div>
             )
           })
         }
-      </header>
+      </div>
     </div>
   );
 }
