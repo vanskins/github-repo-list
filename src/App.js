d748779574798c0e4cd4b634d509e7d8a993f4d7
Van Alfred <vansabacajan@gmail.com>
2022-06-15 00:11:25 +0800

    improve styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import moment from 'moment'
 import './App.css';
 import Api from './api'
 
@@ -6,18 +7,20 @@ import Api from './api'
 function App() {
   const [state, setState] = useState({
     repo: '',
-    repositories: []
+    repositories: [],
+    isLoading: false
   })
   const fetchRepositories = () => {
+    setState({ ...state, isLoading: true})
     Api().get(`/search/repositories?q=${state.repo}`)
     .then(success => {
-      console.log(success)
       if (success && success.request && success.request.status === 200) {
-        setState({ ...state, repositories: success.data.items})
+        setState({ ...state, repositories: success.data.items, isLoading: false})
       }
     })
     .catch(err => {
       console.log(err)
+      setState({ ...state, isLoading: false})
     })
   }
   return (
@@ -29,18 +32,33 @@ function App() {
           const { value } = e.target
           setState({ ...state, repo: value })
         }} placeholder="repository name" />
-        <button className={state.repo.length > 0 ? "button": "button-disabled"} disabled={!state.repo.length > 0} onClick={() => fetchRepositories()}>Submit</button>
+        <button className={state.repo.length > 0 ? "button": "button-disabled"} disabled={!state.repo.length > 0} onClick={() => fetchRepositories()}>{state.isLoading ? "LOADING":"SUBMIT"}</button>
         </div>
       </header>
       <div className="repo-container">
       {
           state.repositories.map((item, key) => {
             return (
-            <div className="repo-card" key={key}>
-              <p>Repository: <b>{item.full_name}</b></p>
-              <p>Forks ({item.forks})</p>
-              <p>Stars ({item.stargazers_count})</p>
-              <p>Click to open repository </p>
+            <div className={key % 2 === 0? "repo-card" : "repo-card-inverted"} key={key}>
+              <img className="avatar" src={item.owner.avatar_url} alt={item.owner.avatar_url} width={100} />
+              <span style={{ textAlign: "center", marginTop: '10px' }}><a style={{ fontWeight: "700", background: "teal", color: "white", padding: '5px', borderRadius: "50px", textDecoration: "none"}} href={item.owner.html_url} target="_blank" rel="noreferrer noopener">@{item.owner.login}</a></span>
+              <p><b>#{key+1}</b></p>
+              <span>Repository: <b>{item.full_name}</b></span>
+              <span style={{ fontSize: '14px', fontWeight: '600'}}>{moment(item.created_at).fromNow()}</span>
+              {
+                item.description &&
+                <p>
+                <span>
+                <i>"{item.description}"</i>
+                </span>
+              </p>
+              }
+              
+              <p>
+                <span style={{ fontSize: '14px', fontWeight: '700', color: "white", borderRadius: '50px', padding: '5px', background: "black", marginRight: '2px'}}>FORKS ({item.forks})</span>
+                <span style={{ fontSize: '14px', fontWeight: '700', color: "black", borderRadius: '50px', padding: '5px', background: "yellow"}}>STARS ({item.stargazers_count})</span>
+                </p>
+              <p>Click link to open repository </p>
               <a href={item.html_url} target="_blank" rel="noreferrer noopener">{item.html_url}</a>
             </div>
             )
